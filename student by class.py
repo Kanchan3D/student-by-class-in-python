f49be865a7a63cb7c2d396bec9a3fc34d70005a6
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-05-08 22:13:50 +0530

    Add files via upload

---
+++ student by class.py
@@ -0,0 +1,10 @@
+class student:
+    #construcutor
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+#creating objects
+s1=student("jessa",20)
+s2=student("Kelly",22)
+print(s1.name)
+print(s1.age)
